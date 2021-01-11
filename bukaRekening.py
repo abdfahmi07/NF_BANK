0544002e3d2863f2ade5392821e6376a8125a2ea
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2021-01-11 13:06:40 +0700

    add feature buka rekening and add file nasabah.txt

--- bukaRekening.py
+++ bukaRekening.py
@@ -1,9 +1,41 @@
 import menu
+import random, string
 
 def buka():
     # silahkan isi logika anda di dalam fungsi ini
-    print("Ini Menu Buka Rekening")
-    print()
+    print(end="\n")
+    print("*** BUKA REKENING ***")
+
+    listNasabah = []
+
+    nasabah = open('nasabah.txt')
+    for element in nasabah:
+        data = element.split(',')
+        rekNum = data[0]
+        nama = data[1]
+        balance = data[2]
+        listNasabah.append([rekNum, nama, balance])
+    nasabah.close()
+
+    nama = input("Masukkan Nama: ")
+    setoranAwal = int(input('Masukkan nominal yang akan disetor: '))
+    norek = "REK" + ''.join(random.choice(string.digits) for _ in range(3))
+    listNasabah.append([norek, nama, setoranAwal])
+    
+    nasabah = open('nasabah.txt', 'w+')
+    itemElement = str(listNasabah).replace('[','').replace(']','').replace("'",'').replace(' ','')
+    nasabah.write(itemElement + '\n')
+    nasabah.close()
+
+    print(end='\n')
+
+
+    
+
+
+
+
+
 
     # balik dan cetak list menu kembali untuk memilih menu,
     # jika menu ini sudah selesai dilakukan.
